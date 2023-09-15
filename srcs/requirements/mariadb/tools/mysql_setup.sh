cdb4207c527f17425effab753df9f4787f618aa4
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-09-15 21:49:38 +0100

    add certificate locations to environment variables add wordpress secrets to environment variables fix volumes not in the required location

--- srcs/requirements/mariadb/tools/mysql_setup.sh
+++ srcs/requirements/mariadb/tools/mysql_setup.sh
@@ -1,7 +1,7 @@
 service mariadb start
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;"
-mariadb-admin -u root -p$SQL_ROOT_PASSWORD --wait-for-all-slaves shutdown
+mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -uroot -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -uroot -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';FLUSH PRIVILEGES;"
+mariadb-admin -uroot -p$SQL_ROOT_PASSWORD --wait-for-all-slaves shutdown
 mysqld_safe
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', getenv('SQL_DATABASE') );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', getenv('SQL_USER') );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', getenv('SQL_PASSWORD') );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', getenv('SQL_HOST') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'f8*i3q&08F,`^&,+,C&!udZ`K`_dvB F8*n(tQDF?kOG45pN*/]&fPr1q_0}+IX]');
-define('SECURE_AUTH_KEY',  '@IkQEjO)Sf7H|xI*UJ;9 Z-xi(nh_/+BU_A,?gy?6V_8clt{f_>j;BL--^73Dk^J');
-define('LOGGED_IN_KEY',    'LVe0}8+Xp <`[`(h_6E*iC2(}hRu%?d2ni0;ok>-@H*!Y[l_C-8o~|>|o#hO>~)|');
-define('NONCE_KEY',        'r_7^3P;Jd2$88#MXoe.f5+nQO~Z^BD*i(Z!? <+>uk)?m>^{+)4$Q}*67PiwGn@K');
-define('AUTH_SALT',        'L]hT{^.@|GC$J12`/9I.<x{iswu6>R<GoQ$<;S3gp]=L[m@_>|mCaEu/@|_dx^iY');
-define('SECURE_AUTH_SALT', 'HMEW;cC^Ap};OVi#4U~Bq}vJpEeN47 KSdLdJW+)^$S<U{(x*+|~7VOqtHs#irOV');
-define('LOGGED_IN_SALT',   'Nbxc! `rv0aEGnB9p1VaU|q%Gh=wj4RdML-Co0d!~ihCOEmKG)|IB0QyR$~;>=JM');
-define('NONCE_SALT',       '$c>^:BdIo]<+vV(6UKsc0%=yYHSw4&+A6*/*0Xth)!{?c0@<OP{44f^Md;Qir`ij');
+define( 'AUTH_KEY',         getenv('WP_AUTH_KEY') );
+define( 'SECURE_AUTH_KEY',  getenv('WP_SECURE_AUTH_KEY') );
+define( 'LOGGED_IN_KEY',    getenv('WP_LOGGED_IN_KEY') );
+define( 'NONCE_KEY',        getenv('WP_NONCE_KEY') );
+define( 'AUTH_SALT',        getenv('WP_AUTH_SALT') );
+define( 'SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT') );
+define( 'LOGGED_IN_SALT',   getenv('WP_LOGGED_IN_SALT') );
+define( 'NONCE_SALT',       getenv('WP_NONCE_SALT') );
 
 /**#@-*/
 
